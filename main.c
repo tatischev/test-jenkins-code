f2f364606a94ca43f7a49c1985f640a41e9029bc
Victor Tatischev <vtatisch@gmail.com>
2020-07-10 02:05:15 +0300

    Fix text warning

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv){
     printf("Jenkins test code\n");
-    if(argv[1] == 1)
+    if(0 == strcmp(argv[1], "1"))
         return -1;
     return 0;
 }
